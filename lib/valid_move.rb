bb3e9de5a9b6c07677bcd4e245207c0142898062
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-05-21 21:37:00 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,10 +5,10 @@ def valid_move?(board, index)
 end
 
 
-def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil
-    false
-  else board[index] == "X" || board[index] == "O"
-    true
-  end
-end
+# def position_taken?(board, index)
+#   if board[index] == " " || board[index] == "" || board[index] == nil
+#     false
+#   else board[index] == "X" || board[index] == "O"
+#     true
+#   end
+# end
